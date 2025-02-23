e09653502753cc3b18fd974f65fe015f2239a6fa
Toby Lopez <sudo.user.tlopez@gmail.com>
2025-02-23 18:32:12 +0000

    Update uv.config.js

--- app/uv/uv.config.js
+++ app/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
-    prefix: '/app/login/',
-    bare: "https://sysadmin.us.kg/",
+    prefix: '/app/v4/',
+    bare: "https://nauka.politechnika-nova.edu.pl/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/app/uv/uv.handler.js',
